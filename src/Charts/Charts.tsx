29749e57347f958b5333533752b740c05814d8f4
dmitryloschinin <loschinin@list.ru>
2022-09-17 18:27:17 +0300

    adding charts in settings

--- src/Charts/Charts.tsx
+++ src/Charts/Charts.tsx
@@ -1,65 +1,41 @@
 import React, { useState } from 'react';
 import './Chars.css';
-import { Chart, Data } from '../Chart/Chart';
+import { Chart } from '../Chart/Chart';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Picker } from '../Picker/Picker';
-
-const mockData1: Data[] = [
-  {
-    date: '2022-09-14T01:32:21.196Z',
-    value: 3,
-  },
-  {
-    date: '2022-09-15T01:32:21.196Z',
-    value: 4,
-  },
-];
-
-const mockData2: Data[] = [
-  {
-    date: '2022-09-11T01:32:21.196Z',
-    value: 2,
-  },
-  {
-    date: '2022-09-12T01:32:21.196Z',
-    value: 3,
-  },
-];
+import {initialDates1, initialDates2, mockData} from "../mockData";
 
 export const Charts = () => {
-  const [selectedDates, setSelectedDates] = useState<
-    string[]
-  >(mockData1.map(data => data.date));
+  const [selectedDates, setSelectedDates] =
+    useState<string[]>(initialDates1);
 
-  const [selectedDates2, setSelectedDates2] = useState<
-    string[]
-  >(mockData2.map(data => data.date));
+  const [selectedDates2, setSelectedDates2] =
+    useState<string[]>(initialDates2);
 
-
-  return mockData1.length /* || mockData2.length*/ ? (
-    <div className={'container'}>
+  return mockData['chart1'].seriesData.length || mockData['chart2'].seriesData.length ? (
+    <div className={'charts-page'}>
       <h1>On this page you can change the date on the charts</h1>
       <div className={'chart-with-picker'}>
         <Picker
           setSelected={setSelectedDates}
-          dates={mockData1.map(data => data.date)}
+          dates={initialDates1}
         />
         <Chart
           chartName={'Chart1'}
           selectedDates={selectedDates}
-          values={mockData1.map(data => data.value)}
+          values={mockData['chart1'].seriesData.map(data => data.value)}
         />
       </div>
-       <div className={'chart-with-picker'}>
-         <Picker
-             setSelected={setSelectedDates2}
-             dates={mockData2.map(data => data.date)}
-         />
-         <Chart
-             chartName={'Chart2'}
-             selectedDates={selectedDates2}
-             values={mockData2.map(data => data.value)}
-         />
+      <div className={'chart-with-picker'}>
+        <Picker
+          setSelected={setSelectedDates2}
+          dates={initialDates2}
+        />
+        <Chart
+          chartName={'Chart2'}
+          selectedDates={selectedDates2}
+          values={mockData['chart2'].seriesData.map(data => data.value)}
+        />
       </div>
     </div>
   ) : (
